88f1c23a160e74b7f53cfb7573c81637824de359
rubymaharjan <113155583+ruby222001@users.noreply.github.com>
2023-05-12 15:35:54 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//delegate int NumberChanger(int n);
+//class Tester
+//{
+//    static int num = 10;
+//    public static int AddNum(int p)
+//    {
+//        num += p;
+//        return num;
+//    }
+//    public static int MulNum(int q)
+//    {
+//        num *= q;
+//        return num;
+//    }
+//    public static int getNum()
+//    {
+//        return num;
+//    }
+
+    
+// static void Main(string[] args)
+//        {
+           
+//            NumberChanger nc1 = new NumberChanger(AddNum);
+//        NumberChanger nc2 = new NumberChanger(MulNum);
+
+        
+//            nc1(4);
+//        Console.WriteLine("value of num {0}", getNum());
+//        nc2(10);
+//        Console.WriteLine("{0}", getNum());
+            
+       
+//        Console.ReadKey();
+//        }
+//    }
+
---
+++ delegate2.cs
@@ -0,0 +1,43 @@
+﻿delegate int NumberChanger(int n);
+class Tester
+{
+    static int num = 10;
+    public static int AddNum(int p)
+    {
+        num += p;
+        return num;
+    }
+    public static int MulNum(int q)
+    {
+        num *= q;
+        return num;
+    }
+    public static int DivNum(int r)
+    {
+        num /= r;
+        return num;
+    }
+    public static int getNum()
+    {
+        return num;
+    }
+
+
+    static void Main(string[] args)
+    {
+
+        NumberChanger nc;
+        NumberChanger nc1 = new NumberChanger(AddNum);
+        NumberChanger nc2 = new NumberChanger(MulNum);
+        NumberChanger nc3 = new NumberChanger(DivNum);
+
+        nc = nc1;
+        nc -= nc2;
+        nc += nc3;
+        nc(10);
+        Console.WriteLine("{0}", getNum());
+
+
+        Console.ReadKey();
+    }
+}
